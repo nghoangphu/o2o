3645671b2cf2d4eb0dde64b876e0fdda29db4659
Hoang Phu <nghoangphu@gmail.com>
2016-11-28 06:41:04 +0000

    Update user model

--- server/boot/sample-models.js
+++ server/boot/sample-models.js
@@ -3,25 +3,32 @@ module.exports = function(app) {
   var Role = app.models.Role;
   var RoleMapping = app.models.RoleMapping;
 
-	User.create([
-		{username: 'Hoang Phu', email: 'nghoangphu@gmail.com', password: 'hoangphu'},
-		{username: 'Phuong Lan', email: 'phuongzlan@gmail.com', password: 'phuonglan'}
-	], function(err, users) {
-	  if (err) throw err;
-    console.log('Created users:', users);
-    users[0].jobs.create({
-      serviceName: 'Service name for Phu',
-      serverFirstName: 'Server first name for Phu'
-    }, function(err, job) {
-      if (err) throw err;
-      console.log('Create job: ', job);
-    })
-    users[1].jobs.create([
-      {serviceName: 'Service name 1 for Lan', serverFirstName: 'Server first name 1 for Lan'},
-      {serviceName: 'Service name 2 for Lan', serverFirstName: 'Server first name 2 for Lan'}
-    ], function(err, job) {
-      if (err) throw err;
-      console.log('Create job: ', job);
-    })  
-	})
+  User.create(
+	  {username: 'Hoang Phu', email: 'nghoangphu@gmail.com', password: 'hoangphu'},
+    function(err, user) {
+	    if (err) throw err;
+      console.log('Created user:', user);
+      user.jobs.create({
+        serviceName: 'Service name for Phu',
+        serverFirstName: 'Server first name for Phu'
+      }, function(err, job) {
+        if (err) throw err;
+        console.log('Create job: ', job);
+      })
+    }
+  );
+  User.create(
+	  {username: 'Phuong Lan', email: 'phuongzlan@gmail.com', password: 'phuonglan'},
+    function(err, user) {
+	    if (err) throw err;
+      console.log('Created user:', user);
+      user.jobs.create([
+        {serviceName: 'Service name 1 for Lan', serverFirstName: 'Server first name 1 for Lan'},
+        {serviceName: 'Service name 2 for Lan', serverFirstName: 'Server first name 2 for Lan'}
+      ], function(err, job) {
+        if (err) throw err;
+        console.log('Create job: ', job);
+      })
+    }
+  );
 }
